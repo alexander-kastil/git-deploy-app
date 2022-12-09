bf7857b23dba0ea2d8e92c4c56fe4124c5399a40
Alexander Pajer <alexander.pajer@integrations.at>
2022-12-10 00:41:22 +0100

    tokens

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 (function () {
     console.log('Checking for easy auth user');
-    try {
-        fetch('/.auth/me')
-            .then((response) => response.json())
-            .then((data) => {
-                document.getElementById('noauth').style.display = 'none';
-                console.log('response: ', data);
-                if (data[0]) {
-                    const msg = `${data[0].user_id}!<br/>Open F12 Dev Tools to see your access token.`;
-                    console.log('Access token: ', data[0].access_token);
-                    document.getElementById('auth').innerHTML = msg;
-                }
-            });
-    } catch (error) {
-        document.getElementById('auth').style.display = 'none';
-    }
+    fetch('/.auth/me')
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById('noauth').style.display = 'none';
+            console.log('response: ', data);
+            if (data[0]) {
+                const msg = `${data[0].user_id}!<br/>Open F12 Dev Tools to see your tokens.`;
+                console.log('ID token: ', data[0].id_token);
+                console.log('Access token: ', data[0].access_token);
+                document.getElementById('auth').innerHTML = msg;
+            }
+        })
+        .catch(() => {
+            const div = document.getElementById('auth');
+            if (div) {
+                div.style.display = 'none';
+            }
+        });
 })();
