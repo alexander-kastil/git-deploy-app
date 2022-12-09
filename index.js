824e5b5ce8396271c700fbe7b75931bf3dfabce0
Alexander Pajer <alexander.pajer@integrations.at>
2022-12-10 00:11:54 +0100

    style

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ function getuser() {
                 document.getElementById('noauth').style.display = 'none';
                 console.log('response: ', data);
                 if (data[0]) {
-                    const msg = `Hello ${data[0].user_id}!<br/> This is your access token:  ${data[0].access_token}.`;
+                    const msg = `${data[0].user_id}!<br/> This is your access token:  ${data[0].access_token}.`;
                     document.getElementById('auth').innerHTML = msg;
-                    // console.log('user: ', data[0].user_id);
-                    // console.log('token: ', data[0].access_token);
                 }
             });
     } catch (error) {
