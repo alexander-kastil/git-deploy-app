ec0fb4972f1027ae6b0d7dd23e5bfb256c700244
Alexander Pajer <alexander.pajer@integrations.at>
2022-12-09 23:59:15 +0100

    auth

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ getuser();
 
 function getuser() {
     console.log('Checking for easy auth user');
-    fetch('/.auth/me')
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('response: ', data);
-            if (data[0]) {
-                console.log('token: ', data[0].access_token);
-                console.log('user: ', data[0].user_id);
-            }
-        });
+    try {
+        fetch('/.auth/me')
+            .then((response) => response.json())
+            .then((data) => {
+                document.getElementById('noauth').style.display = 'none';
+                console.log('response: ', data);
+                if (data[0]) {
+                    console.log('user: ', data[0].user_id);
+                    console.log('token: ', data[0].access_token);
+                }
+            });
+    } catch (error) {
+        document.getElementById('auth').style.display = 'none';
+    }
 }
